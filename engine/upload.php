699cef57ea1adab0884622f0dd5033160cde9569
skmercur <khoudoursofiane75@gmail.com>
2018-04-09 22:19:44 +0100

    seconde version now its working
    we still need to add more security layers

--- engine/upload.php
+++ engine/upload.php
@@ -1,8 +1,12 @@
 <?php
 $directory = "../8742f1fdc2cb59b96955e6c3bfc63debf0324a4f2255435c82cd792c74d7f26a1be60359d28af47aaab02e5bd454810097842bca2676d8ff535cfe56c951b4f4/";
-$file =  $directory.basename($_FILES["file"]["name"]);
-$imageExt = strtolower(pathinfo($file,PATHINFO_EXTENSION));
-if(isset($_POST["submit"])) {
-  move_uploaded_file($_FILES["file"]["tmp_name"], $file);
+$file =  $directory.basename($_FILES["fileto"]["name"]);
+$pathparts = pathinfo($file);
+$ext =  $pathparts["extension"];
+if($ext == "jpg" || $ext == "png" || $ext == "pdf" || $ext == "doc" || $ext == "docx" || $ext == "xls") {
+
+  move_uploaded_file($_FILES["fileto"]["tmp_name"], $file);
+}else{
+  echo "Your file is not supported";
 }
 ?>
