99894a6e6740a34cd4430783fad15419bbd57b0b
mnsDZ <33765559+mnsDZ@users.noreply.github.com>
2018-04-11 02:29:32 +0200

    Add files via upload

---
+++ update-act.php
@@ -0,0 +1,45 @@
+<?php
+if($_GET){
+$id = $_GET['id'];
+		$con = mysqli_connect("localhost", "root", "", "univedu2");
+		$sql = "SELECT * FROM actuallite WHERE id =  ".$id;
+			$r=mysqli_query($con, $sql);
+			$l = mysqli_fetch_array($r);           
+			}
+			?>
+<?php
+if($_POST){
+$titre=$_POST['titre'];
+$date=$_POST['date'];	
+$contenu=$_POST['contenu'];
+		$con = mysqli_connect("localhost", "root", "", "univedu2");
+		$sql = "UPDATE actuallite  set titre='$titre',date='$date',contenu='$contenu' WHERE id='$id'";
+			$r=mysqli_query($con, $sql);
+			$l = mysqli_fetch_array($r);           
+			header("location: show-act.php");
+		}
+			?>
+<form method ="POST" action="#">
+ <table>
+ <tr>
+<td>titre</td>
+<td><input type="text" name="titre" value="<?php echo $l['titre']  ;  ?>" maxlength="30"/>
+</tr>
+ <tr>
+<td>date</td>
+<td><input type="text" name="date" value="<?php echo $l['date']  ;  ?>" maxlength="30"/>
+</td>
+</tr>
+<tr>
+<td>contenu</td>
+<td><input type="text" name="contenu" value="<?php echo $l['contenu']  ;  ?>" maxlength="30"/>
+</td>
+</tr>
+<tr>
+<td colspan="2" align="center">
+<input type = "submit" value="modifier">
+</td>
+</tr>
+</table>
+
+</form>
