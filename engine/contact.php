f6756189c6e3326e99455bf81f60f34c1eb417ed
skmercur <khoudoursofiane75@gmail.com>
2018-04-15 22:49:33 +0100

    version 0

---
+++ engine/contact.php
@@ -0,0 +1,28 @@
+<?php
+if (isset($_POST["submit"])) {
+  $email = $_POST["email"];
+  $text = $_POST["textC"];
+  if (!empty($email) && !empty($text)) {
+
+  }else {
+    echo "Please enter an email and the discription";
+  }
+}
+
+ ?>
+<html>
+<head>
+  <meta charset="utf-8" />
+</head>
+<body>
+
+  <form action="#" method="post" >
+<table>
+  <th>** Bienvenu **</th>
+  <tr><td>Votre email pour que l'ensegnient peut vous contacter </td> <td><input type="email" name="email" placeholder="votre email" </td></tr>
+  <tr><td><textarea name="textC" placeholder="votre message"> </textarea> </td> </tr>
+  <tr> <td><input type="submit" name="submit" value="envoyer" /> </td></tr>
+</table>
+  </form>
+</body>
+</html>
