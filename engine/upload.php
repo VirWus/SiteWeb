8b03e824a89bfc160046b5b5995b2d7bf959e1eb
skmercur <khoudoursofiane75@gmail.com>
2018-04-17 23:16:36 +0100

    updates

--- engine/upload.php
+++ engine/upload.php
@@ -5,19 +5,20 @@ $nom = base64_encode($_POST["nom"]);
 $resumer = base64_encode($_POST["resumer"]);
 $annee = $_POST["annee"];
 $type = $_POST["type"];
+$module = base64_encode($_POST["module"]);
 $link = base64_encode($_POST["link"]);
 $pathparts = pathinfo($file);
 
-if(!empty($nom) && !empty($resumer) && !empty($annee) && !empty($type)  ) {
+if(!empty($nom) && !empty($resumer) && !empty($annee) && !empty($type) && !empty($module)  ) {
   if(!empty($link)){
 include_once("connect/connection.php");
-$query = mysqli_query($connect,"INSERT INTO support VALUES('','$nom','$annee','$resumer','$type','$link')");
+$query = mysqli_query($connect,"INSERT INTO support VALUES('','$nom','$module','$annee','$resumer','$type','$link')");
   }else{
     $ext =  $pathparts["extension"];
 if($ext == "jpg" || $ext == "png" || $ext == "pdf" || $ext == "doc" || $ext == "docx" || $ext == "xls") {
   move_uploaded_file($_FILES["fileto"]["tmp_name"], $file);
   include_once("connect/connection.php");
-  $query = mysqli_query($connect,"INSERT INTO support VALUES('','$nom','$annee','$resumer','$type','$file')");
+  $query = mysqli_query($connect,"INSERT INTO support VALUES('','$nom','$module','$annee','$resumer','$type','$file')");
 }else{
   echo "Your file is not supported";
 }
