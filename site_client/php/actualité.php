b27d67e8a41c1f7a19231a3e333925784a986a5c
VirWus <walidvirwus@github.com>
2018-04-19 00:07:10 +0100

    Site

--- site_client/php/actualité.php
+++
@@ -1,9 +0,0 @@
-<?php 
-$titre=$_POST["titre"];
-$date=$_POST["date"];
-$contenu=$_POST["contenu"];
-$lien=$_POST["lien"];
-$conn = mysqli_connect('localhost','root','','univedu'); 
-mysqli_query ($conn,'SELECT * FROM actuallite WHERE (titre,date,contenu,lien) value("'.$titre.'","'.$date.'","'.$contenu.'","'$lien'")');
-
- ?>
--- univedu/engine/connect/connection.php
+++
@@ -1,3 +0,0 @@
-<?php
-$connect = mysqli_connect("localhost","root","","univedu") or die(mysqli_connect_error());
- ?>
