d1393f3d23828409e811ae3dea7ff9bd9b42f357
skmercur <khoudoursofiane75@gmail.com>
2018-04-21 00:23:03 +0100

    this one might work

--- engine/contact.php
+++ engine/contact.php
@@ -20,35 +20,35 @@ $mail->isSMTP();
 // 0 = off (for production use)
 // 1 = client messages
 // 2 = client and server messages
-$mail->SMTPDebug = 0;
+$mail->SMTPDebug = 1;
 //Set the hostname of the mail server
-$mail->Host = 'smtp.gmail.com';
+$mail->Host = 'smtp.aol.com';
 // use
 // $mail->Host = gethostbyname('smtp.gmail.com');
 // if your network does not support SMTP over IPv6
 //Set the SMTP port number - 587 for authenticated TLS, a.k.a. RFC4409 SMTP submission
-$mail->Port = 587;
+$mail->Port = 465;
 //Set the encryption system to use - ssl (deprecated) or tls
-$mail->SMTPSecure = 'tls';
+$mail->SMTPSecure = 'SSL';
 //Whether to use SMTP authentication
 $mail->SMTPAuth = true;
 //Username to use for SMTP authentication - use full email address for gmail
-$mail->Username = "CSMIFBBA@gmail.com";
+$mail->Username = 'csmibba@aol.com';
 //Password to use for SMTP authentication
-$mail->Password = "CSMI2018";
+$mail->Password = 'mathinformatique2018';
 //Set who the message is to be sent from
-$mail->setFrom('CSMIFBBA@gmail.com', 'support');
+$mail->setFrom('csmibba@aol.com', 'support');
 //Set an alternative reply-to address
 $mail->addReplyTo('khoudoursofiane75@gmail.com', 'support');
 //Set who the message is to be sent to
-$mail->addAddress('b.moussaoui@univ-bba.dz', 'Moussaoui Boubaker');
+$mail->addAddress('walidvirwus@gmail.com', 'Moussaoui Boubaker');
 //Set the subject line
 $mail->Subject = $name.' Vous a envoyer un email avec ce sujet ' . $subject;
 $mail->Body = $text;
 //Read an HTML message body from an external file, convert referenced images to embedded,
 //convert HTML into a basic plain-text alternative body
 //Replace the plain text body with one created manually
-$mail->AltBody = $text;
+
 //Attach an image file
 //send the message, check for errors
 if (!$mail->send()) {
