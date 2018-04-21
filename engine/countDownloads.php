34961c78ee9520380c2f9a126ff60ee349ed4270
skmercur <khoudoursofiane75@gmail.com>
2018-04-21 23:53:49 +0100

    now it works

--- engine/countDownloads.php
+++ engine/countDownloads.php
@@ -1,8 +1,11 @@
 <?php
-$link = base64_encode($_GET["q"]);
+$link = $_GET["q"];
 include_once("connect/connection.php");
+$link = base64_encode("../".$link);
+
 $query = mysqli_query($connect,"UPDATE support SET count = count + 1  WHERE lien = '$link'");
 $li = base64_decode($link);
+$li =  substr($li,3);
 header("Location:http://localhost/SiteWeb/$li");
 
 
