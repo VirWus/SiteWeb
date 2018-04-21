97d80c0368cf4c00e47cb1774d2bf179678e12e7
VirWus <walidvirwus@github.com>
2018-04-22 00:37:57 +0100

    Site Finale v1
    Version 1

--- admin/test.php
+++
@@ -1,9 +0,0 @@
-<?php
-include_once("engine/connect/connection.php");
-$query = mysqli_query($connect,"SELECT * FROM support");
-while ($row = mysqli_fetch_array($query)) {
-  $name[] = $row["name"];
-}
-$res = array($name);
-echo json_encode($res);
- ?>
