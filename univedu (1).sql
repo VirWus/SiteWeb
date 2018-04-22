408e73bd7cffa0c160357d413075414993f2713d
HichemMerniz <38223636+HichemMerniz@users.noreply.github.com>
2018-04-23 00:22:54 +0100

    bd

---
+++ univedu (1).sql
@@ -0,0 +1,145 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.9
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1
+-- Généré le :  lun. 23 avr. 2018 à 01:22
+-- Version du serveur :  10.1.31-MariaDB
+-- Version de PHP :  7.2.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données :  `univedu`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `actuallite`
+--
+
+CREATE TABLE `actuallite` (
+  `id` int(11) NOT NULL,
+  `titre` varchar(50) NOT NULL,
+  `date` text NOT NULL,
+  `contenu` text CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `lien` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `actuallite`
+--
+
+INSERT INTO `actuallite` (`id`, `titre`, `date`, `contenu`, `lien`) VALUES
+(1, 'poo actualite', '30/04/2014', 'cour de pricipe de programmation oriente objet (salle 21) a 9:30 Am', 'www.google.com');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `admin`
+--
+
+CREATE TABLE `admin` (
+  `id` int(11) NOT NULL,
+  `nom` varchar(15) NOT NULL,
+  `prenom` varchar(15) NOT NULL,
+  `date` text NOT NULL,
+  `lieu_de_n` varchar(50) NOT NULL,
+  `secteur` varchar(30) NOT NULL,
+  `univ` varchar(30) NOT NULL,
+  `grade` varchar(20) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `username` varchar(30) NOT NULL,
+  `mdp` text NOT NULL,
+  `bio` text NOT NULL,
+  `image` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `admin`
+--
+
+INSERT INTO `admin` (`id`, `nom`, `prenom`, `date`, `lieu_de_n`, `secteur`, `univ`, `grade`, `email`, `username`, `mdp`, `bio`, `image`) VALUES
+(1, 'Moussaoui', 'boubakeur', '11544', 'Bordj Bou Arreridj', 'mi', 'BBa', 'Docteur', 'Hichembba97@gmail.com', 'Hichem Merniz', '9b71d224bd62f3785d96d46ad3ea3d73319bfbc2890caadae2dff72519673ca72323c3d99ba5c11d7c7acc6e14b8c5da0c4663475c2e5c3adef46f73bcdec0433a2a87e770442585430550b174f476df', 'hfhfkdjqsfgufgmqfgqu', 'Li4vaW1nLzIwMTQxMTI4XzEyMDEyNy5qcGc=');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `support`
+--
+
+CREATE TABLE `support` (
+  `id` int(11) NOT NULL,
+  `nom` text CHARACTER SET utf8 COLLATE utf8_polish_ci NOT NULL,
+  `annee` text NOT NULL,
+  `resumer` text NOT NULL,
+  `type` varchar(10) NOT NULL,
+  `lien` text NOT NULL,
+  `Module` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `support`
+--
+
+INSERT INTO `support` (`id`, `nom`, `annee`, `resumer`, `type`, `lien`, `Module`) VALUES
+(7, 'Hichem', 'Master', 'ddddddddd', '1', '../8742f1fdc2cb59b96955e6c3bfc63debf0324a4f2255435c82cd792c74d7f26a1be60359d28af47aaab02e5bd454810097842bca2676d8ff535cfe56c951b4f4/Cour WEB ---bba--.pdf', '2');
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `actuallite`
+--
+ALTER TABLE `actuallite`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `admin`
+--
+ALTER TABLE `admin`
+  ADD UNIQUE KEY `id` (`id`);
+
+--
+-- Index pour la table `support`
+--
+ALTER TABLE `support`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `actuallite`
+--
+ALTER TABLE `actuallite`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT pour la table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT pour la table `support`
+--
+ALTER TABLE `support`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
