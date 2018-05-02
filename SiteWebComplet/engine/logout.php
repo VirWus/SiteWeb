c41649e0db233080ef0a50baa79128672051771c
VirWus <walidvirwus@github.com>
2018-05-02 22:57:52 +0100

    Walo

--- SiteWebComplet/engine/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-  session_start();
-  $_SESSION['email'] = "";
-  $_SESSION['pass'] = "";
-  session_destroy();
-header('Location:../index.php');
- ?>
--- engine/connect/connection.php
+++
@@ -1,4 +0,0 @@
-<?php
-$connect = mysqli_connect("localhost","root","","univedu") or die(mysqli_connect_error());
-mysqli_query($connect,"SET NAMES UTF8");
- ?>
